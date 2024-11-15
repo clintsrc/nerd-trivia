367c24a182eb73e890f8b8223c915634d166afb3
Rob Grabowski <robert.grabowski1992@gmail.com>
2024-11-14 19:55:51 -0700

    added working localstorage to store username on page one, and it will populate and load on page 2. Once game is complete, it will store the username and the score for the round.

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,21 +1,56 @@
-const carousel = document.querySelector('.carouselbox');
-const prev = document.querySelector('.carouselPrev');
-const next = document.querySelector('.carouselNext');
 
+// This is only storing the username in localstorage and displaying it. This is assuming we are doing another page for the game (which we are)
+// Testing to see how to carry this over to another page
+const usernameDisplay = document.querySelector('.displayName');
+const usernameInput = document.getElementById('username');
+const usernameForm = document.getElementById('usernameForm');
 
-prev.addEventListener('click', (event) => {
-    // Prevent default navigation
-    event.preventDefault();
+function saveUsernameToLocalStorage(username){
   
-    // Do something
-    console.log('prev Link clicked!');
-});
+    localStorage.setItem('username', username);
+}
 
+function loadUsernameFromLocalStorage() {
+    const storedUsername = localStorage.getItem('username'); 
+    if (storedUsername) {
+        usernameDisplay.innerText = storedUsername; 
+    }
+}
 
-next.addEventListener('click', (event) => {
-    // Prevent default navigation
-    event.preventDefault();
-  
-    // Do something
-    console.log('next Link clicked!');
+function handleSubmission(e) {
+    e.preventDefault();
+    const username = usernameInput.value.trim();
+    if (username){
+        saveUsernameToLocalStorage(username);
+        usernameDisplay.innerText = username;
+        usernameInput.value = ''; //clears the current submit from the display
+    } else {
+        alert("Can't be blank, nice try.")
+    }}
+
+usernameForm.addEventListener('submit', handleSubmission);
+
+window.onload = loadUsernameFromLocalStorage;
+
+// This will handle the page redirect for every carousel image (made minor adjustments to index.html. plx review)
+
+document.addEventListener('DOMContentLoaded', function () {
+    const carouselImages = document.querySelectorAll('#carouselExampleIndicators .carousel-item img');
+
+    carouselImages.forEach(image => {
+        image.addEventListener('click', handleImageClick);
+    });
+
+    function handleImageClick(event) {
+        event.preventDefault();
+        const redirectUrl = event.target.getAttribute('data-redirect');
+        const username = localStorage.getItem('username');
+
+        if (!username) {
+            alert("Even alpha Chads have a name. Enter a username to proceed.")
+            
+        } else if (redirectUrl){
+            window.location.href = redirectUrl;
+        }
+    }
 });
